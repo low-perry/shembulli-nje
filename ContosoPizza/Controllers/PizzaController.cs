9ccbb7a2a4f142c28a2521710482ef1ab8510e70
low-perry <dariohaxhiraj@hotmail.com>
2023-12-13 16:29:33 +0100

    Finished the Post, Put, Delete verbs on the controller

--- ContosoPizza/Controllers/PizzaController.cs
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -27,9 +27,38 @@ public class PizzaController : ControllerBase
         return pizza;
     }
 
-    // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {            
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
 
-    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+        return BadRequest();
+           
+        var existingPizza = PizzaService.Get(id);
+        if(existingPizza is null)
+            return NotFound();
+    
+        PizzaService.Update(pizza);           
+    
+        return NoContent();
+    }
 
-    // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+   
+        if (pizza is null)
+            return NotFound();
+        
+        PizzaService.Delete(id);
+    
+        return NoContent();
+    }
 }
